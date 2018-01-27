346693c32a996eedddcc1bdb65faae121a2755b8
Aditya Kumar Sinha <aditya.sinha85@yahoo.com>
2018-01-28 02:39:39 +0800

    Update server.R

--- Developing_Data_Products/Course-Project-Shiny-Application-and-Reproducible-Pitch/server.R
+++ Developing_Data_Products/Course-Project-Shiny-Application-and-Reproducible-Pitch/server.R
@@ -1,47 +1,34 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-library(ggplot2)
-library(dplyr)
-# select columns to be used in the analysis
-diam <- diamonds[,c(1:4,7)]
-# Define server logic required to draw a plot
+
+# Load data
+data <- read.csv("individuals-fake-data-p1.csv", nrows = 5000, header = TRUE)
+data$Gender <- as.factor(data$Gender)
+
+# Create shiny server
 shinyServer(function(input, output) {
-    output$distPlot <- renderPlot({
-        # select diamonds depending of user input
-        diam <- filter(diamonds, grepl(input$cut, cut), grepl(input$col, color), grepl(input$clar, clarity))
-        # build linear regression model
-        fit <- lm( price~carat, diam)
-        # predicts the price 
-        pred <- predict(fit, newdata = data.frame(carat = input$car,
-                                                  cut = input$cut,
-                                                  color = input$col,
-                                                  clarity = input$clar))
-        # Drow the plot using ggplot2
-        plot <- ggplot(data=diam, aes(x=carat, y = price))+
-            geom_point(aes(color = cut), alpha = 0.3)+
-            geom_smooth(method = "lm")+
-            geom_vline(xintercept = input$car, color = "red")+
-            geom_hline(yintercept = pred, color = "green")
-       plot
-    })
-    output$result <- renderText({
-        # renders the text for the prediction below the graph
-        diam <- filter(diamonds, grepl(input$cut, cut), grepl(input$col, color), grepl(input$clar, clarity))
-        fit <- lm( price~carat, diam)
-        pred <- predict(fit, newdata = data.frame(carat = input$car,
-                                                  cut = input$cut,
-                                                  color = input$col,
-                                                  clarity = input$clar))
-        res <- paste(round(pred, digits = 2), "$")
-        res
-    })
+  
+  # Data filtering reaction on input parameters
+  dataFilter <- reactive({
+    paste0("data[data$Age > ", input$age[1], " & data$Age < ", input$age[2], 
+        ifelse(input$gender != "all", paste0(" & data$Gender == '", input$gender, "'"), ""), ", c(2,5,6,7,",
+        ifelse(input$email == TRUE, "15,", ""),
+        ifelse(input$phone == TRUE, "19,", ""), "23) ]")
+  })
+  
+  # Data filtering
+  output$dataTable <- renderDataTable({
+    eval(parse(text = dataFilter()))
+  })
+  
+  # Data filtering as a caption
+  output$caption <- renderText({
+    dataFilter()
+  })
+  
+  # Plot rendering
+  output$contactsPlot <- renderPlot({
+    data <- eval(parse(text = dataFilter()))
+    boxplot(data$Age ~ data$Gender, data = data, xlab = "Gender", ylab = "Age", col = c("red", "blue"))
+  })
   
 })
