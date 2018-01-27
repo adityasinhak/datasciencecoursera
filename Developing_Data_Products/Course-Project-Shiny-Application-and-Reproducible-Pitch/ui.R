9d0c2aeea5d6ebffc0fd0f5d062f6746174b29d4
Aditya Kumar Sinha <aditya.sinha85@yahoo.com>
2018-01-28 02:40:33 +0800

    Update ui.R

--- Developing_Data_Products/Course-Project-Shiny-Application-and-Reproducible-Pitch/ui.R
+++ Developing_Data_Products/Course-Project-Shiny-Application-and-Reproducible-Pitch/ui.R
@@ -1,46 +1,35 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+# Define UI for Fake Contacts Generator application
+shinyUI(pageWithSidebar(
   
   # Application title
-  titlePanel("Diamond price prediction"),
+  headerPanel("Fake Contacts Generator by Aditya Sinha"),
   
-  # Sidebar with options selectors 
-  sidebarLayout(
-    sidebarPanel(
-        helpText("This application predicts the price of a diamond based on its characteristics."),
-        h3(helpText("Select:")),
-       numericInput("car", label = h4("Carats"), step = 0.01, value = 2),
-       selectInput("cut", label = h4("Cut"), 
-                   choices = list("Unknown" = "*", "Fair" = "Fair", "Good" = "^Good",
-                                  "Very Good" = "Very Good", "Premium" = "Premium",
-                                  "Ideal" = "Ideal")),
-       selectInput("col", label = h4("Color"), 
-                   choices = list("Unknown" = "*", "D" = "D", "E" = "E",
-                                  "F" = "F", "G" ="G",
-                                  "H" = "H", "I" = "I",
-                                  "J" = "J")),
-       selectInput("clar", label = h4("Clarity"), 
-                   choices = list("Unknown" = "*", "I1" = "I1", "SI2" = "SI2",
-                                  "SI1" = "SI1", "VS2" = "VS2", "VS1" = "VS1",
-                                  "VVS2" = "VVS2", "VVS1" = "VVS1", "IF" = "IF" ))
-    ),
+  # Left panel with control elements for Fake Contacts Generator
+  sidebarPanel(
+    h5("Please, choose gender for generator..."),
+    selectInput("gender", "Gender:",
+                list("All" = "all", 
+                     "Male" = "male", 
+                     "Female" = "female")),
     
-    # Show a plot with diamonds and regression line
-    mainPanel(
-       plotOutput("distPlot"),
-       h4("Predicted value of this diamond is:"),
-       h3(textOutput("result"))
-    )
+    h5("... and age range"),
+    sliderInput("age", "Age:",
+                min = 18, max = 65, value = c(25,45)),
+    
+    checkboxInput("email", "Show email in final list", FALSE),
+    checkboxInput("phone", "Show phone in final list", FALSE)
+    
+    
+  ),
+  
+  # Right panel with Fake Contacts List as table and bar chart for distribution
+  mainPanel(
+    h3("Fake Contacts"),
+    dataTableOutput('dataTable'),
+    h3("Fake Contacts by Age and Gender"),
+    h5(textOutput("caption")),
+    plotOutput("contactsPlot")
   )
 ))
